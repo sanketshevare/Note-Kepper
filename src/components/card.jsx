7bb1899ccc860ce9a988863b8da086e4d2cf88bb
sanketshevare <sanketshevare7675@gmail.com>
2023-07-30 14:36:34 +0530

    Fixed Memory Issue

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,10 +1,17 @@
-import React, { useEffect, useState } from 'react';
-import ReactPaginate from 'react-paginate';
-import { BsFillPinAngleFill } from 'react-icons/bs';
-import { RiDeleteBinFill } from 'react-icons/ri';
-import { GrEdit } from 'react-icons/gr';
+import React, { useEffect, useState } from "react";
+import ReactPaginate from "react-paginate";
+import { BsFillPinAngleFill } from "react-icons/bs";
+import { RiDeleteBinFill } from "react-icons/ri";
+import { GrEdit } from "react-icons/gr";
 import { firestore } from "../config/firebase";
-import { collection, getDocs, deleteDoc, doc, updateDoc } from "firebase/firestore";
+import {
+  collection,
+  getDocs,
+  deleteDoc,
+  doc,
+  updateDoc,
+} from "firebase/firestore";
+import { Tooltip as ReactTooltip } from "react-tooltip";
 
 export default function PaginatedItems({ itemsPerPage }) {
   const [note, setNote] = useState([]);
@@ -30,7 +37,10 @@ export default function PaginatedItems({ itemsPerPage }) {
   const getNotes = async () => {
     try {
       const querySnapshot = await getDocs(collection(firestore, "notes"));
-      const newData = querySnapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id }));
+      const newData = querySnapshot.docs.map((doc) => ({
+        ...doc.data(),
+        id: doc.id,
+      }));
       setNote(newData);
       console.log(newData);
     } catch (error) {
@@ -43,7 +53,6 @@ export default function PaginatedItems({ itemsPerPage }) {
   }, []);
 
   // Delete functionality
-
   const handleDelete = async (id) => {
     try {
       await deleteDoc(doc(collection(firestore, "notes"), id));
@@ -53,18 +62,16 @@ export default function PaginatedItems({ itemsPerPage }) {
     }
   };
 
-  // Edit functionality 
+  // Edit functionality
   const [editingItemId, setEditingItemId] = useState(null);
-  const [editedTitle, setEditedTitle] = useState('');
-  const [editedBody, setEditedBody] = useState('');
+  const [editedTitle, setEditedTitle] = useState("");
+  const [editedBody, setEditedBody] = useState("");
 
   const startEditing = (itemId, title, body) => {
     setEditingItemId(itemId);
     setEditedTitle(title);
     setEditedBody(body);
   };
-
-
 
   const handleSaveEdit = async (id) => {
     try {
@@ -75,8 +82,8 @@ export default function PaginatedItems({ itemsPerPage }) {
       // Fetch the updated list of notes after saving the edits
       getNotes();
       setEditingItemId(null);
-      setEditedTitle('');
-      setEditedBody('');
+      setEditedTitle("");
+      setEditedBody("");
     } catch (error) {
       console.log("Error updating document: ", error);
     }
@@ -85,99 +92,138 @@ export default function PaginatedItems({ itemsPerPage }) {
   const handleCancelEdit = () => {
     // Clear the edit state
     setEditingItemId(null);
-    setEditedTitle('');
-    setEditedBody('');
+    setEditedTitle("");
+    setEditedBody("");
   };
 
   return (
-    <div className='flex flex-col '>
-      <div className='grid md:grid-cols-3 '>
-        {currentItems.sort((a, b) => (pinned.includes(a.id) ? -1 : pinned.includes(b.id) ? 1 : 0)).map((item) => (
-          <div key={item.id} className='hover:-translate-y-6 ease-in duration-300 '>
-            <div className={`bg-slate-200 text-black  m-5 rounded-lg shadow-lg ${editingItemId === item.id ? 'border-2 border-blue-500' : ''}`}>
-              <div className='grid grid-cols-2'>
-                <BsFillPinAngleFill
-                  className={`text-4xl hover:rotate-12 ${pinned.includes(item.id) ? 'text-green-900' : 'text-red-600'} m-1 cursor-pointer`}
-                  onClick={() => pin(item.id)}
-                />
+    <div className="flex flex-col ">
+      <div className="grid md:grid-cols-3 ">
+        {currentItems
+          .sort((a, b) =>
+            pinned.includes(a.id) ? -1 : pinned.includes(b.id) ? 1 : 0
+          )
+          .map((item) => (
+            <div
+              key={item.id}
+              className="hover:-translate-y-6 ease-in duration-300 "
+            >
+              <div
+                className={`bg-slate-200 text-black  m-5 rounded-lg shadow-lg ${
+                  editingItemId === item.id ? "border-2 border-blue-500" : ""
+                }`}
+              >
+                <div className="grid grid-cols-2">
+                  <BsFillPinAngleFill
+                  data-tooltip-id="pintoTop"
+                    className={`text-4xl hover:rotate-12 focus:outline-none ${
+                      pinned.includes(item.id)
+                        ? "text-green-900"
+                        : "text-red-600"
+                    } m-1 cursor-pointer`}
+                    onClick={() => pin(item.id)}
+                  />
+                  <ReactTooltip
+                    id="pintoTop"
+                    place="bottom"
+                    content="Pin To Top"
+                    variant="info"
+                  />
+                  <p className="text-right m-1 ">{item.createdAt}</p>
+                </div>
+                {editingItemId === item.id ? (
+                  <>
+                    <input
+                      className="text-xl uppercase bg-transparent outline-none"
+                      value={editedTitle}
+                      onChange={(e) => setEditedTitle(e.target.value)}
+                    />
+                    <div className="h-px bg-slate-600 w-full"></div>
+                    <textarea
+                      className="p-2 bg-transparent outline-none"
+                      rows={4}
+                      value={editedBody}
+                      onChange={(e) => setEditedBody(e.target.value)}
+                    />
+                    <div className="grid grid-cols-2 justyfy-items-center">
+                      <button
+                        className="p-3 bg-blue-500"
+                        onClick={() => handleSaveEdit(item.id)}
+                      >
+                        Save
+                      </button>
+                      <button
+                        className="p-3 bg-red-500"
+                        onClick={handleCancelEdit}
+                      >
+                        Cancel
+                      </button>
+                    </div>
+                  </>
+                ) : (
+                  <>
+                    <center>
+                      <p className="text-xl uppercase">{item.title}</p>
+                    </center>
+                    <div className="h-px bg-slate-600 w-full"></div>
 
-                <p className='text-right m-1 '>{item.createdAt}</p>
+                    <div className="flex">
+                      {item.tag ? (
+                        <p className="p-1 bg-sky-800 rounded-xl text-xs text-white m-1  uppercase ">
+                          {item.tag}
+                        </p>
+                      ) : null}
+                    </div>
+                    <div className="p-24">
+                      <p>{item.body}</p>
+                    </div>
+                    <div className="grid grid-cols-2 justyfy-items-center border-t-2 border-black">
+                      <button
+                        className="p-1 bg-blue-400"
+                        onClick={() =>
+                          startEditing(item.id, item.title, item.body)
+                        }
+                      >
+                        <center>
+                          <GrEdit />
+                        </center>
+                        Edit
+                      </button>
+                      <button
+                        className="p-1 bg-red-400 align-center"
+                        onClick={() => handleDelete(item.id)}
+                      >
+                        <center>
+                          <RiDeleteBinFill className="" />
+                        </center>
+                        Delete
+                      </button>
+                    </div>
+                  </>
+                )}
               </div>
-              {editingItemId === item.id ? (
-                <>
-                  <input
-                    className="text-xl uppercase bg-transparent outline-none"
-                    value={editedTitle}
-                    onChange={(e) => setEditedTitle(e.target.value)}
-                  />
-                  <div className='h-px bg-slate-600 w-full'></div>
-                  <textarea
-                    className="p-2 bg-transparent outline-none"
-                    rows={4}
-                    value={editedBody}
-                    onChange={(e) => setEditedBody(e.target.value)}
-                  />
-                  <div className='grid grid-cols-2 justyfy-items-center'>
-                    <button className="p-3 bg-blue-500" onClick={() => handleSaveEdit(item.id)}>
-                      Save
-                    </button>
-                    <button className="p-3 bg-red-500" onClick={handleCancelEdit}>
-                      Cancel
-                    </button>
-                  </div>
-                </>
-              ) : (
-                <>
-                  <center>
-                    <p className='text-xl uppercase'>{item.title}</p>
-                  </center>
-                  <div className='h-px bg-slate-600 w-full'></div>
-
-                  <div className='flex'>
-                    {item.tag ? <p className='p-1 bg-sky-800 rounded-xl text-xs text-white m-1  uppercase '>{item.tag}</p> : null}
-                  </div>
-                  <div className='p-24'>
-                    <p>{item.body}</p>
-                  </div>
-                  <div className='grid grid-cols-2 justyfy-items-center border-t-2 border-black'>
-                    <button className='p-1 bg-blue-400' onClick={() => startEditing(item.id, item.title, item.body)}>
-                      <center>
-                        <GrEdit />
-                      </center>
-                      Edit
-                    </button>
-                    <button className='p-1 bg-red-400 align-center' onClick={() => handleDelete(item.id)}>
-                      <center>
-                        <RiDeleteBinFill className='' />
-                      </center>
-                      Delete
-                    </button>
-                  </div>
-                </>
-              )}
             </div>
-          </div>
-        ))}
+          ))}
       </div>
 
-      <div className=''>
+      <div className="">
         <ReactPaginate
-          className='flex flex-row m-5 justify-center'
-          breakLabel='...'
-          nextLabel='>'
+          className="flex flex-row m-5 justify-center"
+          breakLabel="..."
+          nextLabel=">"
           onPageChange={(event) => {
             const newOffset = (event.selected * itemsPerPage) % note.length;
             setItemOffset(newOffset);
           }}
           pageRangeDisplayed={5}
           pageCount={pageCount}
-          previousLabel='<'
+          previousLabel="<"
           renderOnZeroPageCount={null}
           previousClassName="mr-6 text-fuchsia-200"
           nextClassName="ml-6 text-fuchsia-200"
           pageLinkClassName="m-5 text-white"
-          activeClassName='bg-blue-500 text-yellow-400 rounded-2xl p-1 font-bold'
-          disabledClassName='text-gray-700'
+          activeClassName="bg-blue-500 text-yellow-400 rounded-2xl p-1 font-bold"
+          disabledClassName="text-gray-700"
         />
       </div>
     </div>
--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,144 +1,128 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useRef, useState } from "react";
 import { firestore } from "../config/firebase";
-import { addDoc, collection, getDocs } from "firebase/firestore";
+import { addDoc, collection } from "firebase/firestore";
 import PaginatedItems from "./card";
 
 export default function Home() {
+  var newDate = new Date();
+  var datetime =     new Date().toLocaleString();
+  ;
 
-    let currentdate = new Date(); 
-    let datetime = currentdate.getDate() + "/"
-                    + (currentdate.getMonth()+1)  + "/" 
-                    + currentdate.getFullYear() + " @ "  
-                    + currentdate.getHours() + ":"  
-                    + currentdate.getMinutes();
+console.log(datetime);
+  const titleRef = useRef();
+  const bodyRef = useRef();
+  const tagRef = useRef();
 
+  const ref = collection(firestore, "notes");
 
-    const titleRef = useRef();
-    const bodyRef = useRef();
-    const tagRef = useRef();
+  // State variables for validation
+  const [titleError, setTitleError] = useState("");
+  const [bodyError, setBodyError] = useState("");
 
-    const [note, setNote] = useState([]);
-    const ref = collection(firestore, "notes");
+  const handleSave = async (e) => {
+    e.preventDefault();
 
-    // State variables for validation
-    const [titleError, setTitleError] = useState("");
-    const [bodyError, setBodyError] = useState("");
+    // Reset previous validation errors
+    setTitleError("");
+    setBodyError("");
 
-    const handleSave = async (e) => {
-        e.preventDefault();
+    const titleValue = titleRef.current.value.trim();
+    const bodyValue = bodyRef.current.value.trim();
+    const tagValue = tagRef.current.value.trim();
 
-        // Reset previous validation errors
-        setTitleError("");
-        setBodyError("");
+    // Check if title and body are not empty
+    if (!titleValue) {
+      setTitleError("Title is required.");
+      return;
+    }
 
-        const titleValue = titleRef.current.value.trim();
-        const bodyValue = bodyRef.current.value.trim();
-        const tagValue = tagRef.current.value.trim();
+    if (!bodyValue) {
+      setBodyError("Body is required.");
+      return;
+    }
 
-        // Check if title and body are not empty
-        if (!titleValue) {
-            setTitleError("Title is required.");
-            return;
-        }
-
-        if (!bodyValue) {
-            setBodyError("Body is required.");
-            return;
-        }
-
-        let data = {
-            title: titleValue,
-            body: bodyValue,
-            createdAt: datetime,
-            tag: tagValue,
-        };
-
-        try {
-            await addDoc(ref, data);
-            // Clear input fields after successful save
-            titleRef.current.value = "";
-            bodyRef.current.value = "";
-            tagRef.current.value = "";
-
-            // Fetch the updated list of notes after a successful save
-            getNotes();
-            window.location.href = 'https://notekeeper-2d7ef.web.app/';
-        } catch (e) {
-            console.log(e);
-        }
+    let data = {
+      title: titleValue,
+      body: bodyValue,
+      createdAt: datetime,
+      tag: tagValue,
     };
 
-    const getNotes = async () => {
-        try {
-            const querySnapshot = await getDocs(collection(firestore, "notes"));
-            const newData = querySnapshot.docs.map((doc) => ({
-                ...doc.data(),
-                id: doc.id,
-            }));
-            setNote(newData);
-            console.log(newData);
-           
-        } catch (error) {
-            console.log("Error getting documents: ", error);
-        }
-    };
+    try {
+      await addDoc(ref, data);
+      // Clear input fields after successful save
+      titleRef.current.value = "";
+      bodyRef.current.value = "";
+      tagRef.current.value = "";
 
-    // Fetch the initial list of notes
-    useEffect(() => {
-        getNotes();
-    }, []);
+      // Fetch the updated list of notes after a successful save
+      // getNotes();
+      window.location.href = "https://notekeeper-2d7ef.web.app/";
+    } catch (e) {
+      console.log(e);
+    }
+  };
 
-    return (
-        <div className=" bg-[url('../public/bg.jpg')] bg-center bg-no-repeat bg-cover flex flex-col items-center justify-center shadow-4xl">
-            <h1 className="text-4xl font-bold p-3 text-blue-700 bg-lime-300  w-full">Note-Keeper</h1>
-            <div className="md:w-1/3 w-11/12 m-1 mb-5">
-                <form
-                    onSubmit={handleSave}
-                    className="p-5 mt-10 bg-gray-500 flex flex-col rounded-lg shadow-lg"
-                >
-                    <h1 className="text-3xl text-lime-400 font-bold mb-1">Add New Note:</h1>
-                    <input
-                        type="text"
-                        placeholder="Title..."
-                        ref={titleRef}
-                        className="p-3 focus:outline-none w-full rounded-md"
-                    />
-                    {titleError && <p className="text-cyan-300 text-md">{titleError}</p>}
-                    <br />
-                    <textarea
-                        type="text"
-                        placeholder="Take Note Here..."
-                        ref={bodyRef}
-                        rows={7}
-                        cols={23}
-                        className="p-3 w-full focus:outline-none rounded-md"
-                    />
-                    {bodyError && <p className="text-cyan-300 text-md">{bodyError}</p>}
 
-                    <br />
-                    <input
-                        type="text"
-                        placeholder="Tag..."
-                        ref={tagRef}
-                        className="p-3 focus:outline-none w-full rounded-md"
-                        
-                    />
-         <br />
+  // Fetch the initial list of notes
 
-                    <button
-                        type="submit"
-                        className="p-4 bg-amber-500 text-white rounded-md text-xl font-bold w-full"
-                    >
-                        Save
-                    </button>
-                </form>
-            </div>
 
-            <div className="">
-                <div className="">
-                    <PaginatedItems itemsPerPage={6} />
-                </div>
-            </div>
+  return (
+    <div className=" bg-[url('../public/bg.jpg')] bg-center bg-no-repeat bg-cover flex flex-col items-center justify-center shadow-4xl">
+      <div className="sticky top-0 w-full z-50">
+        <h1 className="text-4xl font-bold p-3 text-blue-700 bg-lime-300  w-full">
+          Note-Keeper
+        </h1>
+      </div>
+      <div className="md:w-1/3 w-11/12 m-1 mb-5">
+        <form
+          onSubmit={handleSave}
+          className="p-5 mt-10 bg-gray-500 flex flex-col rounded-lg shadow-lg"
+        >
+          <h1 className="text-3xl text-lime-400 font-bold mb-1">
+            Add New Note:
+          </h1>
+          <input
+            type="text"
+            placeholder="Title..."
+            ref={titleRef}
+            className="p-3 focus:outline-none w-full rounded-md"
+          />
+          {titleError && <p className="text-cyan-300 text-md">{titleError}</p>}
+          <br />
+          <textarea
+            type="text"
+            placeholder="Take Note Here..."
+            ref={bodyRef}
+            rows={7}
+            cols={23}
+            className="p-3 w-full focus:outline-none rounded-md"
+          />
+          {bodyError && <p className="text-cyan-300 text-md">{bodyError}</p>}
+
+          <br />
+          <input
+            type="text"
+            placeholder="Tag..."
+            ref={tagRef}
+            className="p-3 focus:outline-none w-full rounded-md"
+          />
+          <br />
+
+          <button
+            type="submit"
+            className="p-4 bg-amber-500 text-white rounded-md text-xl font-bold w-full"
+          >
+            Save
+          </button>
+        </form>
+      </div>
+
+      <div className="">
+        <div className="">
+          <PaginatedItems itemsPerPage={6} />
         </div>
-    );
+      </div>
+    </div>
+  );
 }
